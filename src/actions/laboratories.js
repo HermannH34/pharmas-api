a113074f211127daeee8efbdc72beba995029df6
Hermann Hairet <hermannhairet44@gmail.com>
2023-02-03 14:13:29 +0100

    test

--- src/actions/laboratories.js
+++ src/actions/laboratories.js
@@ -1,8 +1,7 @@
 import { db } from '../database.js'
-import { faker } from '@faker-js/faker';
 
 export const listLabs = (req, res) => {
   const labs = db.prepare('SELECT * FROM laboratories').all();
 
-  res.send(faker.address.city())
+  res.send(labs)
 }
