357b25cdaaf642b8b396c8f0577f2b75b8f00545
Hermann Hairet <hermannhairet44@gmail.com>
2023-02-07 17:08:06 +0100

    seed for products + refacto list pharmas by city

--- src/actions/pharmacies.js
+++ src/actions/pharmacies.js
@@ -3,12 +3,12 @@ import { db } from '../database.js'
 export const listPharms = (req, res) => {
   const pharms = db.prepare('SELECT * FROM pharmacies').all();
 
-  res.send(JSON.stringify(pharms))
+  res.send(pharms);
 }
 
 
 export const listByCity = (req, res) => {
   const pharms = db.prepare('SELECT * FROM pharmacies WHERE city = ?').all(req.params.city);
 
-  res.send(JSON.stringify(pharms.map(pharm => pharm["name"])))
+  res.send(pharms.map(pharm => pharm["name"]));
 }
--- src/seed.js
+++ src/seed.js
@@ -1,13 +1,15 @@
 import { db } from './database.js'
 import { faker } from '@faker-js/faker';
 
+db.prepare("DELETE FROM products").run()
 db.prepare("DELETE FROM pharmacies").run()
 db.prepare("DELETE FROM laboratories").run()
 
-// create pharmas
-let city = ["Glasgow", "London", "Manchester", "Plymouth"];
 
-for (let i = 0; i < 25; i++) {
+// create pharmas
+let cities = ["Glasgow", "London", "Manchester", "Plymouth"];
+
+for (const city of cities) {
   db.prepare("INSERT INTO pharmacies (name, city) VALUES (?, ?)").run(
     faker.company.name(), city[Math.floor(Math.random() * city.length)]
   )
@@ -18,10 +20,29 @@ console.log("Pharmacies created !")
 // create labo
 const labs = ["Servier", "J&J", "Pfizer"]
 
-for (let i = 0; i < labs.length; i++) {
+for (const lab of labs) {
   db.prepare("INSERT INTO laboratories (name) VALUES (?)").run(
-    labs[i]
+    lab
   )
 }
 
 console.log("Laboratories created !")
+
+// create products
+const products = ["Doliprane", "Smecta", "Efferalgan", "Xanax", "Lexomil", "Dafalgan"]
+
+for (let i = 0; i < 50; i++) {
+  const ids = db.prepare("SELECT id FROM laboratories").all()
+
+  const rangeOfId = ids.map(id => id.id)
+
+  const labId = db.prepare("SELECT id FROM laboratories WHERE id = ?").get(rangeOfId[Math.floor(Math.random() * rangeOfId.length)])
+
+  // créer un procuct et lui affecter un labo
+  db.prepare("INSERT INTO products (name, laboratoryid) VALUES (?, ?)").run(
+    products[Math.floor(Math.random() * products.length)],
+    labId.id
+  )
+}
+
+console.log("Products created !")
