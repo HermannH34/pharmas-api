a577f346ed07f288c457b290aae5c910cdead007
Hermann Hairet <hermannhairet44@gmail.com>
2023-02-19 19:40:16 +0100

    endpoint for create a pharma v1

--- src/actions/createpharma.js
+++ src/actions/createpharma.js
@@ -2,6 +2,13 @@ import { db } from '../database.js'
 
 export const createPharma = (req, res) => {
   // verifier si user est un pharmacien
-  console.log(req.user)
-  // if (req.user || !req.user.ispharmacy) return res.status(401).send('invalid credentials')
+  if (!req.user || req.user.ispharmacy == 'false') return res.status(401).send('invalid credentials')
+
+  res.send(db.prepare('INSERT INTO pharmacies (name, city) VALUES (?, ?)').run(
+    req.body.name, req.body.city
+  ))
 }
+
+// checker si pharmacie existe
+// créer un vrai boolean
+// gérer les réponse positives (messages)
