cfcdb793d022a156d81f2c7b0660a4ad9bb2f91d
Hermann Hairet <hermannhairet44@gmail.com>
2023-02-20 12:12:37 +0100

    fix errors messages

--- src/actions/createpharma.js
+++ src/actions/createpharma.js
@@ -2,12 +2,12 @@ import { db } from '../database.js'
 
 export const createPharma = (req, res) => {
   const { name, city } = req.body
-  // verifier si user est un pharmacien ou si pharmacie déjà créee
+  if (!name || !city) return res.status(401).send('invalid body')
+  // verifier si user est un pharmacien ou si pharmacie déjà créée
   const checkPharma = db.prepare('SELECT name FROM pharmacies WHERE name = ?').all(name);
+  if (!req.user || req.user.ispharmacy == 'false' || checkPharma.length !== 0) return res.status(401).send('invalid credentials OR user not a pharmacy OR pharmacy already exist')
 
-  if (!req.user || req.user.ispharmacy == 'false' || checkPharma.length !== 0) return res.status(401).send('invalid credentials OR user not a pharmacy O0 pharmacy already exist')
-
-  res.send(db.prepare('INSERT INTO pharmacies (name, city) VALUES (?, ?)').run(
+  res.status(201).send(db.prepare('INSERT INTO pharmacies (name, city) VALUES (?, ?)').run(
     name, city
   ))
 }
