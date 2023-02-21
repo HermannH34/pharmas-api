4214fcda08c85aaa7aa600e3b4be9dd9d8db0df2
Hermann Hairet <hermannhairet44@gmail.com>
2023-02-21 16:14:34 +0100

    format responses in json

--- src/actions/createpharma.js
+++ src/actions/createpharma.js
@@ -2,12 +2,12 @@ import { db } from '../database.js'
 
 export const createPharma = (req, res) => {
   const { name, city } = req.body
-  if (!name || !city) return res.status(401).send('invalid body')
+  if (!name || !city) return res.status(401).json('invalid body')
   // verifier si user est un pharmacien ou si pharmacie déjà créée
   const checkPharma = db.prepare('SELECT name FROM pharmacies WHERE name = ?').all(name);
-  if (!req.user || req.user.ispharmacy == 'false' || checkPharma.length !== 0) return res.status(401).send('invalid credentials OR user not a pharmacy OR pharmacy already exist')
+  if (!req.user || req.user.ispharmacy == 'false' || checkPharma.length !== 0) return res.status(401).json('invalid credentials OR user not a pharmacy OR pharmacy already exist')
 
-  res.status(201).send(db.prepare('INSERT INTO pharmacies (name, city, userid) VALUES (?, ?, ?)').run(
+  res.status(201).json(db.prepare('INSERT INTO pharmacies (name, city, userid) VALUES (?, ?, ?)').run(
     name, city, req.user.id
   ))
 }
